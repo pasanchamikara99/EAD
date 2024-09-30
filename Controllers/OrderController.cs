66e69095f3ed82b8e5549a8da680f62f261cce8e
IT21016202 <it21016202@my.sliit.lk>
2024-09-30 22:45:41 +0530

    basic api created

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,93 @@
+﻿using E_commerce_system.Data;
+using E_commerce_system.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace E_commerce_system.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMongoCollection<Order>? _orders;
+
+        public OrderController(MongoDbService mongoDbService)
+        {
+            _orders = mongoDbService.Database?.GetCollection<Order>("order");
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Order>> Get()
+        {
+            return await _orders.Find(FilterDefinition<Order>.Empty).ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Order order)
+        {
+            await _orders.InsertOneAsync(order);
+            return CreatedAtAction(nameof(GetById), new { id = order.Id }, order);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order?>> GetById(string id)
+        {
+            var filter = Builders<Order>.Filter.Eq(x => x.Id, id);
+            var order = await _orders.Find(filter).FirstOrDefaultAsync();
+            return order is not null ? Ok(order) : NotFound();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(string id, Order order)
+        {
+            var filter = Builders<Order>.Filter.Eq(x => x.Id, id);
+            if (order is null)
+            {
+                return NotFound();
+            }
+            if (order.Status == "Delivered" || order.Status == "Dispatched")
+            {
+                return BadRequest("Cannot update order");
+            }
+            var update = Builders<Order>.Update
+                .Set(x => x.UserId, order.UserId)
+                .Set(x => x.ShippingAddress, order.ShippingAddress)
+                .Set(x => x.Status, order.Status)
+                .Set(x => x.OrderDate, order.OrderDate)
+                //.Set(x => x.OrderItems, order.OrderItems)
+                .Set(x => x.OrderTotal, order.OrderTotal);
+            var result = await _orders.UpdateOneAsync(filter, update);
+            return result.IsAcknowledged ? Ok(order) : NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(string id)
+        {
+            var filter = Builders<Order>.Filter.Eq(x => x.Id, id);
+            var result = await _orders.DeleteOneAsync(filter);
+            return result.IsAcknowledged ? Ok() : NotFound();
+        }
+
+        //cancel order if not delivered or dispatched
+        [HttpPut("{id}/cancel")]
+        public async Task<ActionResult> CancelOrder(string id)
+        {
+            var filter = Builders<Order>.Filter.Eq(x => x.Id, id);
+            var order = await _orders.Find(filter).FirstOrDefaultAsync();
+            if (order is null)
+            {
+                return NotFound();
+            }
+            if (order.Status == "Delivered" || order.Status == "Dispatched")
+            {
+                return BadRequest("Cannot cancel order");
+            }
+            var update = Builders<Order>.Update.Set(x => x.Status, "Cancelled");
+            var result = await _orders.UpdateOneAsync(filter, update);
+            return result.IsAcknowledged ? Ok() : NotFound();
+
+        }
+
+    }
+}
---
+++ Entities/Order.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace E_commerce_system.Entities
+{
+    public class Order
+    {
+        [BsonId]
+        [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+
+        [BsonElement("user_id"), BsonRepresentation(BsonType.String)]
+        public string? UserId { get; set; }
+
+
+        [BsonElement("shipping_address"), BsonRepresentation(BsonType.String)]
+        public string? ShippingAddress { get; set; }
+
+
+        [BsonElement("status"), BsonRepresentation(BsonType.String)]
+        public string? Status { get; set; }
+
+
+        [BsonElement("order_date"), BsonRepresentation(BsonType.DateTime)]
+        public DateTime OrderDate { get; set; }
+
+
+        [BsonElement("order_total"), BsonRepresentation(BsonType.Decimal128)]
+        public decimal OrderTotal { get; set; }
+
+        //Order Items as an array
+        //[BsonElement("order_items"), BsonRepresentation(BsonType.String)]
+        //public List<string>? OrderItems { get; set; }
+
+    }
+}
