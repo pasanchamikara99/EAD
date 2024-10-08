0ebd9df4290b271f743fe4e33a44a966c1478bee
Amisha Prathyanga <spbamisha@gmail.com>
2024-10-08 15:13:17 +0530

    minor additions

--- obj/Debug/net8.0/E-commerce system.AssemblyInfo.cs
+++ obj/Debug/net8.0/E-commerce system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-commerce system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0b85e35726201b3dd3af50c5960b2c23491fe7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e334e7078f5a6b6ef3b3f5b6f8aad5545fafc4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-commerce system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-commerce system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
